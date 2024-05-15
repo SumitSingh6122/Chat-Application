1de18fc29ebb985a05b5a947239e7fa9939a743d
SumitSingh6122 <159093892+SumitSingh6122@users.noreply.github.com>
2024-05-15 20:56:00 +0700

    Delete chat-app directory

--- chat-app/Backend/Routers/messageRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import {getMessage, sendMessage } from '../controllers/messagecontroller.js';
-import aunthenticated from '../controllers/middleware/aunthenticated.js';
-const Router=express.Router();
-Router.route('/send/:id').post(aunthenticated,sendMessage);
-Router.route('/:id').get(aunthenticated,getMessage);
-export default Router;
--- chat-app/Backend/Routers/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Login, Register, logout, otheruser } from '../controllers/usercontroller.js';
-import aunthenticated from '../controllers/middleware/aunthenticated.js';
-const router=express.Router();
-router.route('/register').post(Register);
-router.route('/login').post(Login);
-router.route('/logout').get(logout);
-router.route('/').get(aunthenticated,otheruser);
-export default router;
