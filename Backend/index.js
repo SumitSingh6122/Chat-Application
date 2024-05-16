6b3dc87bb21776e3dfd00c2f9e824659af16bc1b
SumitSingh6122 <159093892+SumitSingh6122@users.noreply.github.com>
2024-05-16 09:19:31 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption={
-   origin:'http://localhost:3000',
+   origin:'https://chat-application-three-green.vercel.app',
    credentials:true
 }
 app.use(cros(corsOption));
