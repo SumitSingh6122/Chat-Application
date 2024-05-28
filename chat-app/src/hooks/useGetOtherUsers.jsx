4d1972c8d5a19b452e566a76151e7f68e4c97dbc
SumitSingh6122 <159093892+SumitSingh6122@users.noreply.github.com>
2024-05-28 08:33:19 +0700

    Update useGetOtherUsers.jsx

--- chat-app/src/hooks/useGetOtherUsers.jsx
+++ chat-app/src/hooks/useGetOtherUsers.jsx
@@ -10,15 +10,15 @@ const useGetOtherUsers = () => {
     useEffect(() => {
         const fetchOtherUsers = async () => {
             try {
-                // Set Axios configuration for cross-origin requests
+
                 axios.defaults.withCredentials = true;
 
                 const res = await axios.get('https://chat-application-r2bx.onrender.com/api/v1/user');
 
-                // Dispatch the retrieved data to Redux store
+            
                 dispatch(setOthers(res.data));
             } catch (error) {
-                // Handle errors
+            
                 console.error('Error fetching other users:', error);
             }
         };
