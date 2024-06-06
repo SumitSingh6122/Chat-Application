a0f9a647981e8e9d35d0a39b363ff1b08f7565e2
SumitSingh6122 <159093892+SumitSingh6122@users.noreply.github.com>
2024-06-06 11:04:04 +0700

    Update ActiveChat.jsx

--- chat-app/src/components/ActiveChat.jsx
+++ chat-app/src/components/ActiveChat.jsx
@@ -4,14 +4,18 @@ import { useSelector } from 'react-redux';
 const ActiveChat = () => {
   const {ClickUser}=useSelector(store=>store.Users);
   if(!ClickUser)  return;
+  const handleClick=()=>{
+    document.getElementById("3").style.display="none";
+  }
+  
   return (
    
         
-        <div className="profile-container pc ">
-        <div className='icon-container'>
+        <div className="profile-container pc "> <div onClick={handleClick}><i class=' arrow  bx bx-left-arrow-alt'></i></div>
+        <div className='icon-container  mobile-view' > 
   <img src={ClickUser.profilePhoto}  alt=''/>
   <div className='st '>
-  <span>{ClickUser.Fullname}</span>
+  <span>   {ClickUser.Fullname}</span>
   </div>
 
 </div>  </div> 
