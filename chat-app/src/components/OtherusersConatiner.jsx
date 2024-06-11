034779d0e16817a3aafca063c329fa843e55f2d9
SumitSingh6122 <159093892+SumitSingh6122@users.noreply.github.com>
2024-06-11 11:31:43 +0700

    Update OtherusersConatiner.jsx

--- chat-app/src/components/OtherusersConatiner.jsx
+++ chat-app/src/components/OtherusersConatiner.jsx
@@ -8,11 +8,14 @@ import useGetRealTimeMessage from '../hooks/useGetRealTimeMessage';
 import { setAuthenticatedUser } from '../redux/userSlice';
 
 
-const OtherusersConatiner = () => {
+const OtherusersConatiner = ({search}) => {
   const dispatch=useDispatch();
    useGetRealTimeMessage();
    const navigate=useNavigate();
   const {Others}=useSelector(store=>store.Users);
+  if (!Others) {
+    return <div>Loading...</div>; 
+  }
  const handleLogout=async()=>{
 try {
   const res= await axios.get("https://chat-application-r2bx.onrender.com/api/v1/user/logout");
@@ -23,15 +26,20 @@ try {
   console.log(error);
 }
  }
+ const OtherUsersData= search ? Others.filter(user => user?.Fullname?.toLowerCase().includes(search.toLowerCase())) : Others;
+if(!OtherUsersData){
+  toast.error("User not found")
+}
+
   return (
     <div className='otheruser-container'>
-      {
-        Others?.map((user)=>{
-          return(
-            <Otherusers  key={user?._id} user={user}/> 
-          )
-        })
-      }
+     {OtherUsersData.length > 0 ? (
+        OtherUsersData.map(user => (
+          <Otherusers key={user?._id} user={user} />
+        ))
+      ) : (
+        <div>User Not Found</div>
+      )}
 
       <div className="logout item-center flex justify-center "><button className='logout-btn' onClick={handleLogout}>Logout<i class='bx bx-log-out'></i></button></div> 
     </div>
